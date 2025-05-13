471c72074d5d146e0881554b32ce265e61f3f24b
mario-leandro <marioleandro2004@gmail.com>
2025-05-13 17:12:37 -0300

    Removendo componente de saldo atual - componente com informacao redundante

--- src/components/SaldoAtual.tsx
+++
@@ -1,9 +0,0 @@
-export default function SaldoAtual() {
-  return (
-    <div className="w-full h-auto rounded-2xl px-3 md:px-5">
-      <div className="flex flex-col justify-center items-center">
-        <p className="text-violet-950 font-bold">Saldo Atual: R$ 0,00</p>
-      </div>
-    </div>
-  );
-}
